d6224a1662b932d8033442540b2f456aa85087a5
pauag09 <pauarenas2004@gmail.com>
2026-02-24 21:12:08 -0600

    creacion de conexion deMysql

--- conexion/src/App.java
+++ conexion/src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+
+        Customer c1 = new Customer(1, "Paulina", "Arenas", 21, "pau.doe@example.com");
+        Customer c2 = new Customer(2, "Alejandro", "cacho", 21, "ale.doe@example.com");
+        
+        MySqlCRUD conn = new MySqlCRUD();
     }
 }
